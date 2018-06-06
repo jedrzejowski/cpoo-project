32f442cd4610e1226e90bb5a44d85a7e613f7743
Adam Jędrzejowski <adam@jedrzejowski.pl>
2018-06-06 23:54:24 +0200

    Nie wiem co :D

--- src/adam/monika/adam/Main.java
+++ src/adam/monika/adam/Main.java
@@ -1,42 +1,38 @@
 package adam.monika.adam;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfFloat;
-import org.opencv.core.MatOfInt;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
+import adam.monika.adam.ui.FrameUI;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 
-public class Main {
+public class Main extends Application {
 
     public static void main(String[] args) {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        launch();
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
 
         File inputFolder = new File("pic/input");
         File[] listOfImages = inputFolder.listFiles();
+        BufferedImage img = null;
 
-        for (File file : listOfImages) {
-            processImage(file);
+        //read image
+        try {
+            img = ImageIO.read(listOfImages[1]);
+
+            new FrameUI(img);
+        } catch (IOException e) {
+            System.out.println(e);
         }
-    }
 
-    private static void processImage(File file) {
-        System.out.println(file.getPath());
-        Mat imageMat = Imgcodecs.imread(file.getPath());
-
-        System.out.println(imageMat.channels());
-
-        ArrayList<Mat> list = new ArrayList<Mat>();
-        list.add(imageMat);
-
-        Mat histogram = new Mat();
-        MatOfInt histSize = new MatOfInt(256);
-        final MatOfFloat histRange = new MatOfFloat(0f, 256f);
-
-        Imgproc.calcHist(list, new MatOfInt(3), new Mat(), histogram, histSize, histRange,false);
     }
 }
